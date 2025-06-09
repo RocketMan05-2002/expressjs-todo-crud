2ed23b9286aa1966328f617456684ab90852b239
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-09 23:31:19 +0530

    add middleware for add todo

--- routers/todo.routes.js
+++ routers/todo.routes.js
@@ -1,33 +1,42 @@
 const express = require("express");
-const { getAllTodos, addTodo, deleteTodoById, updateTodobyId, getTodoById, getTodoByQuery } = require("../controllers/todo.controller");
+const {
+  getAllTodos,
+  addTodo,
+  deleteTodoById,
+  updateTodobyId,
+  getTodoById,
+  getTodoByQuery,
+} = require("../controllers/todo.controller");
 
 const todoRouter = express.Router();
 
-const checkIncomingTodo = (req,res,next) => {
-    const allowedKeys = ["id","name","status"];
-    const keys = Object.keys(req.body);
+const checkIncomingTodo = (req, res, next) => {
+  const allowedKeys = ["id", "name", "status"];
+  const keys = Object.keys(req.body);
 
-    const hasOnlyAllowedKeys = keys.every( key => allowedKeys.includes(key));
-    const hasRequiredKeys = allowedKeys.every(key => key in req.body);
+  const hasOnlyAllowedKeys = keys.every((key) => allowedKeys.includes(key));
+  const hasRequiredKeys = allowedKeys.every((key) => key in req.body);
 
-    if(!hasOnlyAllowedKeys || !hasRequiredKeys){
-        res.status(400).send("Invalid todo format: only 'name' and 'status' are allowed.");
-    }else{
-        next();
-    }
-}
+  if (!hasOnlyAllowedKeys || !hasRequiredKeys) {
+    res
+      .status(400)
+      .send("Invalid todo format: only 'name' and 'status' are allowed.");
+  } else {
+    next();
+  }
+};
 
 // 1. read todos
 todoRouter.get("/get-todos", getAllTodos);
 
 // 2. add a todo
-todoRouter.post("/add-todo", checkIncomingTodo , addTodo );
+todoRouter.post("/add-todo", checkIncomingTodo, addTodo);
 
 //3. delete a todo
 todoRouter.delete("/delete-todo/:id", deleteTodoById);
 
 // 4. update a todo
-todoRouter.put("/update-todo/:id", updateTodobyId );
+todoRouter.put("/update-todo/:id", updateTodobyId);
 
 //5. get todo by id
 todoRouter.get("/todos/:id", getTodoById);
