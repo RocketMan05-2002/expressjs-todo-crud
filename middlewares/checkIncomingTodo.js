b20524d9a435658c262f34d09557ba76c58a95fe
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-11 23:59:59 +0530

    logger middleware check

--- middlewares/checkIncomingTodo.js
+++ middlewares/checkIncomingTodo.js
@@ -1,15 +1,17 @@
-const checkIncomingTodo = (req, res, next) => {
-  const allowedKeys = ["id", "name", "status"];
-  const keys = Object.keys(req.body);
 
-  const hasOnlyAllowedKeys = keys.every((key) => allowedKeys.includes(key));
-  const hasRequiredKeys = allowedKeys.every((key) => key in req.body);
 
-  if (!hasOnlyAllowedKeys || !hasRequiredKeys) {
-    res.status(400).send("invalid todo format");
-  } else {
-    next();
-  }
-};
+const checkIncomingTodo = (req,res,next) => {
+    const allowedKeys = ["id","name","status"];
+    const keys = Object.keys(req.body);
+
+    const hasOnlyAllowedKeys = keys.every(key => allowedKeys.includes(key));
+    const hasRequiredKeys = allowedKeys.every(key => key in req.body);
+
+    if(!hasOnlyAllowedKeys || !hasRequiredKeys){
+        res.status(400).send("invalid todo format");
+    }else{
+        next();
+    }
+}
 
 module.exports = checkIncomingTodo;
