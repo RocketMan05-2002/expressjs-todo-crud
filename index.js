66adb3a64526c75e97e509d2eba6da145e40319f
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-11 16:27:07 +0530

    logger middleware check

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 const fs = require("fs");
 const express= require("express");
 const todoRouter = require("./routers/todo.router");
-const blogRouter = require("./routers/blog.router")
+const blogRouter = require("./routers/blog.router");
+const loggerMiddleware = require("./middlewares/logger");
+
 const app = express();
 app.use(express.json());
+app.use(loggerMiddleware);
 
 //0. test request
 app.get("/test",(req,res)=>{
