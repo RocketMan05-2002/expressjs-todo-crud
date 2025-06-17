a10a02bdfa0d0b1660051780c624921784167d06
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-17 23:39:22 +0530

    format fixes

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,10 +1,12 @@
-const fs = require("fs")
+const fs = require("fs");
 
-const loggerMiddleware = (req,res,next) => {
-    // this middleware just logs all requests in a file
-    let data =`\n End-point: ${req.url} | Method: ${req.method} | Date-Time: ${new Date().toISOString()} \n`;
-    fs.appendFileSync("./logs.txt",data);
-    next();
-}
+const loggerMiddleware = (req, res, next) => {
+  // this middleware just logs all requests in a file
+  let data = `\n End-point: ${req.url} | Method: ${
+    req.method
+  } | Date-Time: ${new Date().toISOString()} \n`;
+  fs.appendFileSync("./logs.txt", data);
+  next();
+};
 
 module.exports = loggerMiddleware;
