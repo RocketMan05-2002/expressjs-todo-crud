0ba5357d66aa4a8c6fc0f465cc4b4d367d9eae55
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-17 23:38:44 +0530

    format fixes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
 const fs = require("fs");
-const express= require("express");
+const express = require("express");
 const todoRouter = require("./routers/todo.router");
 const blogRouter = require("./routers/blog.router");
 const loggerMiddleware = require("./middlewares/logger");
@@ -10,20 +9,20 @@ app.use(express.json());
 app.use(loggerMiddleware);
 
 //0. test request
-app.get("/test",(req,res)=>{
-    res.send("wepbicI");
-})
+app.get("/test", (req, res) => {
+  res.send("wepbicI");
+});
 
 //todos
 app.use("/todos", todoRouter);
 
 //blogs
-app.use("/blogs", blogRouter)
+app.use("/blogs", blogRouter);
 
-app.use((req,res)=>{
-    res.status(400).send("request not found");
-})
+app.use((req, res) => {
+  res.status(400).send("request not found");
+});
 
-app.listen(8000,()=>{
-    console.log("server started via 8000 port");
-})
+app.listen(8000, () => {
+  console.log("server started via 8000 port");
+});
