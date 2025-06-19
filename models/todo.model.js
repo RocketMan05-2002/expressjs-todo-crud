8f273011c8585aaf10ce943b760aaea984ade626
Rocketman <abhinayshambharkar1@gmail.com>
2025-06-19 23:55:37 +0530

    format fixes

--- models/todo.model.js
+++ models/todo.model.js
@@ -1,15 +1,14 @@
-
 const fs = require("fs");
 
 const getTodoData = () => {
-    let data = JSON.parse(fs.readFileSync("./db.json","utf-8"));
-    let todos = data.todos;
+  let data = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
+  let todos = data.todos;
 
-    return {data,todos};
-}
+  return { data, todos };
+};
 
 const addOrUpdateTodo = (data) => {
-    fs.writeFileSync("./db.json",JSON.stringify(data));
-}
+  fs.writeFileSync("./db.json", JSON.stringify(data));
+};
 
-module.exports = {getTodoData, addOrUpdateTodo};
+module.exports = { getTodoData, addOrUpdateTodo };
